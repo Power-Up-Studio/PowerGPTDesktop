28213812ab5a5a04f795269536efac19e5cf16d2
Power Up Studio <76599999+PowerUpStudios@users.noreply.github.com>
2023-07-31 15:57:10 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-PGPTConversation.onMessage((msg) => console.log(msg), window.parent.document)
+PGPTConversation.onMessage((msg) => {
+  if (msg.isGPT) {
+    msg.contentChanged(null, () => {
+      console.log(msg)
+    })
+  }
+}, window.parent.document)
